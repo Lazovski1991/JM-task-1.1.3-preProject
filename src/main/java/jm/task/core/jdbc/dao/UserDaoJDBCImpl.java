2fbf3acc79a89edd6d0a9dc915d89387be9b4e42
Alexander <Lazovski1991@gmail.com>
2021-01-13 14:19:18 +0300

    add rollback

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,10 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,34 +21,48 @@ public class UserDaoJDBCImpl implements UserDao {
                 " last_name VARCHAR (50) not NULL, " +
                 " age INTEGER not NULL, " +
                 " PRIMARY KEY (id));";
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint("Save");
             PreparedStatement psSt = connection.prepareStatement(SQL);
             psSt.executeUpdate();
             connection.commit();
-
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
         String SQL = "DROP TABLE IF EXISTS users;";
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint("Save");
             PreparedStatement psSt = connection.prepareStatement(SQL);
             psSt.executeUpdate();
             connection.commit();
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         String insert = "INSERT INTO " + dbTable + " (name, last_name, age) VALUES (?,?,?)";
-
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint("Save");
             PreparedStatement prST = connection.prepareStatement(insert);
             prST.setString(1, name);
             prST.setString(2, lastName);
@@ -61,18 +72,30 @@ public class UserDaoJDBCImpl implements UserDao {
             connection.commit();
             System.out.println("User с именем " + name + " добавлен в базу данных");
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
         String SQL = "DELETE FROM users WHERE id = " + id;
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint("Save");
             PreparedStatement psSt = connection.prepareStatement(SQL);
             psSt.executeUpdate();
             connection.commit();
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
@@ -80,8 +103,10 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> usersList = new ArrayList<>();
         String s = "SELECT * FROM " + dbTable;
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint("Save");
             PreparedStatement psSt = connection.prepareStatement(s);
             ResultSet resultSet = psSt.executeQuery();
             while (resultSet.next()) {
@@ -94,6 +119,11 @@ public class UserDaoJDBCImpl implements UserDao {
             }
             connection.commit();
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
         return usersList;
@@ -101,12 +131,19 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         String SQL = "DELETE FROM users";
+        Connection connection = Util.getConnection();
+        Savepoint savepoint = null;
         try {
-            Connection connection = Util.getConnection();
+            savepoint = connection.setSavepoint();
             PreparedStatement psSt = connection.prepareStatement(SQL);
             psSt.executeUpdate();
             connection.commit();
         } catch (SQLException throwables) {
+            try {
+                connection.rollback(savepoint);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             throwables.printStackTrace();
         }
     }
